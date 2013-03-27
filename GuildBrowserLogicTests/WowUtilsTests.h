0f4e8b64548274ee8c173f53cca7ef75e186c335
wufulin <wufulinit@gmail.com>
2013-03-27 12:18:44 +0800

    添加GuildBrowser逻辑测试target，对WowUtils类进行单元测试

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by wufulin on 27/3/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
